1187a3bb02104b53802aba3fe106b3fb9061f2a4
sashetty1 <shrivatsa.ashetty@ironmountain.com>
2025-02-27 07:33:07 +0530

    started working on DOM Exercise 2

---
+++ JS-Dom2/script.js
@@ -0,0 +1,10 @@
+console.log("Hello World!!!");
+// console.log(document.body);
+// console.log(document.body.childNodes);
+// console.log(typeof document.body.childNodes);
+// console.log(document.body.childNodes[0]);
+// console.log(document.body.childNodes[1]);
+// console.log(document.body.childNodes[2]);
+// console.log(document.body.childNodes[3]);
+// console.log(document.body.childNodes[1].childNodes);
+
