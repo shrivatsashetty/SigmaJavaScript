5717c62cef892def4280274c57af9ed5139f867f
shrivatsashetty <shrivatsa307@gmail.com>
2025-04-12 20:51:23 +0530

    added handler for double click

--- JS-Events/script.js
+++ JS-Events/script.js
@@ -1,18 +1,12 @@
-btn1 = document.getElementById("btn-1");
+let container1 = document.querySelector(".container-1");
 
-// btn1.addEventListener("click", function (e) {
-//     alert("Button Clicked");
-//     console.log(e); // script.js:5 PointerEvent {isTrusted: true, pointerId: 3, width: 1, height: 1, pressure: 0, …}
-//     console.log("Event Target:", e.currentTarget); // Event Target: <button id=​"btn-1">​Click Me​</button>​
-//     console.log(e.currentTarget === this); // false
-//     console.log(this); // Window {window: Window, self: Window, document: document, name: '', location: Location, …}
-    
-// })
+btn1 = document.getElementById("btn-1");
 
 /* adding a simple event listener */ 
 btn1.addEventListener( "click", (e) => {alert("Hello World!!!")} );
 
-/* adding a detailed event handler */ 
+/* adding a detailed event handler
+to handle a mouse double click event */ 
 btn2 = document.querySelector("#btn-2");
 
 btn2.addEventListener("dblclick", e => {
@@ -23,4 +17,14 @@ btn2.addEventListener("dblclick", e => {
     console.log("this === e.target:", this === e.target);
     console.log("this object:", this);
 });
+
+/* handling a right click event */ 
+btn3 = document.getElementById("btn-3");
+
+btn3.addEventListener(
+    "contextmenu", 
+    e => {
+        alert("btn-3 right clicked!!!")
+    }
+);
 
