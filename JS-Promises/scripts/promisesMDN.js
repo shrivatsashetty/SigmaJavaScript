b3be4b67256a89e18482df1c7d9f812ffd372a6a
shrivatsashetty <shrivatsa307@gmail.com>
2025-05-18 09:29:15 +0530

    added async await

--- JS-Promises/scripts/promisesMDN.js
+++ JS-Promises/scripts/promisesMDN.js
@@ -1,38 +1,54 @@
 /* an arrow function that returns either 0 or 1 */
 const getRandomBool = () => Math.floor(Math.random()*2);
 
-function successCallback(result) {
-    console.log(`Audio file ready at URL: ${result}`);
-}
+/* a function to return a random number within an upper limit */
+const getRandomNum = (upperLimit) => Math.floor(Math.random()*upperLimit);
 
-function failureCallback(error) {
-    console.error(`Error generating audio file: ${error}`);
-}
+function getApiEndPoint() {
+    /* to give a loading effect */
+    console.log("Getting Endpoint...");
 
-function getPromise() {
-    console.log("Processing...");
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (getRandomBool()) {
-                resolve("https://example.com/");
+                resolve(`https://jsonplaceholder.typicode.com/todos/${getRandomNum(200)}`);
             } 
             else {
-                reject("404 Not found!");
+                reject("Error 404: Not found!");
             }
         }, 1500);
     });
 }
 
-// const promise1 = getPromise();
-// const promise2 = promise1.then(successCallback, failureCallback);
+/* 
 
-getPromise()
-    .then((url) => {
-        return fetch(url); // example.com is the url that will be returned
-    })
-    .then((result) => {
-        console.log(result);
+getApiEndPoint()
+    .then((endpointURL) => fetch(endpointURL))
+    .then((response) => response.json())
+    .then((jsonData) => {
+        console.log(jsonData);
     })
     .catch((err) => {
-        console.log(err);
+        console.log("Error:", err);
     });
+
+*/
+
+
+// below is one more way to write the above commeted code using async await which makes it resemble syncronours code
+async function getUserData() {
+
+    try {
+        const url = await getApiEndPoint();
+        const response = await fetch(url);
+        const jsonData = await response.json();
+        console.log(jsonData);
+        return jsonData;
+    }
+    catch (err) {
+        console.log("Error fetching data:", err);
+        throw err;        
+    }
+} 
+  
+getUserData();
