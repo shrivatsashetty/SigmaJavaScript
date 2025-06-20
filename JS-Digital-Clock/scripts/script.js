6342b7b780fb4a9fba6923e9f1636072ab7fad6d
shrivatsashetty <shrivatsa307@gmail.com>
2025-06-20 23:31:32 +0530

    created a digital clock using javascript

---
+++ JS-Digital-Clock/scripts/script.js
@@ -0,0 +1,26 @@
+
+function displayCurrentTime() {
+
+    let date = new Date();
+
+    let currentHour24 = date.getHours(); // will return hour in 24 hrs format
+
+    let currentHour12 = date.getHours() % 12 || 12; // for 12:00 & 24:00 Hrs
+    currentHour12 = currentHour12.toString().padStart(2, "0");
+
+    let currentMinute = date.getMinutes().toString().padStart(2, "0");
+    
+    let currentSecond = date.getSeconds().toString().padStart(2, "0");
+
+    let meridiem = (currentHour24 < 12) ? "AM" : "PM";
+
+    let strCurrentTime = `${currentHour12}:${currentMinute}:${currentSecond} ${meridiem}`;
+
+    // console.log(strCurrentTime); // 8 : 48 : 3 PM
+
+    let clock = document.getElementById("clock");
+    clock.innerText = strCurrentTime;
+}
+
+
+setInterval(displayCurrentTime, 1000);
